1cf95579ade5d074cd11dc8c40b9d7e765d63b8b
Minwoo Lee <ermaker@gmail.com>
2016-01-21 09:21:46 +0000

    Try

--- guess/model.py
+++ guess/model.py
@@ -1,15 +1,42 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.regularizers import l1l2, activity_l1l2
+
 import json
 
 class Model(object):
   def __init__(self):
     self.N = 5
 
-  def load_data(self):
-    with open('data/sequence.json') as f:
+  def load_data(self, filename):
+    with open(filename) as f:
       self.data = json.loads(f.read())
     self.length = len(self.data) - self.N
     self.X = [self.data[idx:idx + self.N] for idx in xrange(self.length)]
     self.Y = self.data[self.N:]
+
+  def train(self):
+    self.model = Sequential()
+    self.model.add(Dense(100, input_dim=self.N, W_regularizer=l1l2(), b_regularizer=l1l2(), activity_regularizer=activity_l1l2()))
+    self.model.add(Activation('relu'))
+    self.model.add(Dropout(0.25))
+    self.model.add(Dense(100, W_regularizer=l1l2(), b_regularizer=l1l2(), activity_regularizer=activity_l1l2()))
+    self.model.add(Activation('relu'))
+    self.model.add(Dropout(0.25))
+    self.model.add(Dense(100, W_regularizer=l1l2(), b_regularizer=l1l2(), activity_regularizer=activity_l1l2()))
+    self.model.add(Activation('relu'))
+    self.model.add(Dropout(0.25))
+    self.model.add(Dense(100, W_regularizer=l1l2(), b_regularizer=l1l2(), activity_regularizer=activity_l1l2()))
+    self.model.add(Activation('relu'))
+    self.model.add(Dropout(0.25))
+    self.model.add(Dense(1))
+    self.model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
+    self.model.fit(np.array(self.X), np.array(self.Y), show_accuracy=True, batch_size=16, nb_epoch=2000, verbose=2)
+
+  def predict(self, X):
+    return self.model.predict(np.array([X]), verbose=0)[0][0]
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,12 @@
 from guess import model
+import numpy as np
 
 import unittest
 
 class ModelTest(unittest.TestCase):
   def test_load_data(self):
     m = model.Model()
-    m.load_data()
+    m.load_data('tests/fixtures/simple_sequence.json')
     data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     self.assertEqual(data, m.data)
 
@@ -29,3 +30,54 @@ class ModelTest(unittest.TestCase):
     ]
     self.assertEqual(X, m.X)
     self.assertEqual(Y, m.Y)
+
+  def test_train_and_predict(self):
+    X = [
+      [1, 2, 3, 4, 5, 6],
+      [2, 3, 4, 5, 6, 7],
+      [3, 4, 5, 6, 7, 8],
+      [4, 5, 6, 7, 8, 9],
+      [5, 6, 7, 8, 9, 10]
+    ]
+
+    m = model.Model()
+    m.load_data('data/sequence.json')
+    m.train()
+    print X[0], m.predict(X[0][:-1])
+    print X[1], m.predict(X[1][:-1])
+    print X[2], m.predict(X[2][:-1])
+    print X[3], m.predict(X[3][:-1])
+    print X[4], m.predict(X[4][:-1])
+    V = [1,2,10,20,30,25]
+    print V, m.predict(V[:-1])
+    V = [23,23,22,21,21,21]
+    print V, m.predict(V[:-1])
+    V = [7,6,7,6,5,4]
+    print V, m.predict(V[:-1])
+    V = [10,9,8,7,6,5]
+    print V, m.predict(V[:-1])
+    V = [14,15,16,17,25,7]
+    print V, m.predict(V[:-1])
+    V = [100,101,102,103,104,105]
+    print V, m.predict(V[:-1])
+    V = [1000,1010,1020,1030,1040,1050]
+    print V, m.predict(V[:-1])
+    V = np.array([1000,1010,1020,1030,1040,1050]) * 10
+    print V, m.predict(V[:-1])
+    V = np.array(X[0]) * 10
+    print V, m.predict(V[:-1])
+    V = np.array(X[0]) * 100
+    print V, m.predict(V[:-1])
+    V = np.array(X[0]) * 1000
+    print V, m.predict(V[:-1])
+    V = np.array(X[1]) * 10
+    print V, m.predict(V[:-1])
+    V = np.array(X[2]) * 10
+    print V, m.predict(V[:-1])
+    V = np.array(X[3]) * 10
+    print V, m.predict(V[:-1])
+    V = np.array(X[4]) * 10
+    print V, m.predict(V[:-1])
+
+    actual = m.predict([1, 2, 3, 4, 5])
+    self.assertEqual(6, actual)
