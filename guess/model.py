6a3fba554ba5b15b724559fbbc8f97f502e471ed
Minwoo Lee <ermaker@gmail.com>
2016-01-21 07:11:49 +0000

    Add Model#X and Model#Y

--- guess/model.py
+++ guess/model.py
@@ -4,6 +4,12 @@ from __future__ import print_function
 import json
 
 class Model(object):
+  def __init__(self):
+    self.N = 5
+
   def load_data(self):
     with open('data/sequence.json') as f:
       self.data = json.loads(f.read())
+    self.length = len(self.data) - self.N
+    self.X = [self.data[idx:idx + self.N] for idx in xrange(self.length)]
+    self.Y = self.data[self.N:]
--- tests/test_model.py
+++ tests/test_model.py
@@ -6,4 +6,26 @@ class ModelTest(unittest.TestCase):
   def test_load_data(self):
     m = model.Model()
     m.load_data()
-    self.assertNotEqual(None, m.data)
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    self.assertEqual(data, m.data)
+
+    self.assertEqual(5, m.N)
+    self.assertEqual(5, m.length)
+
+
+    X = [
+      [1, 2, 3, 4, 5],
+      [2, 3, 4, 5, 6],
+      [3, 4, 5, 6, 7],
+      [4, 5, 6, 7, 8],
+      [5, 6, 7, 8, 9]
+    ]
+    Y = [
+      6,
+      7,
+      8,
+      9,
+      10
+    ]
+    self.assertEqual(X, m.X)
+    self.assertEqual(Y, m.Y)
